04a0d82af7ddd08d8950e5c3c74ed1e4ad001557
dbr <danielbrocha@gmail.com>
2025-03-31 07:59:41 -0400

    .

--- nvim.bak/lua/config/keymaps.lua
+++ nvim.bak/lua/config/keymaps.lua
@@ -1,13 +1,16 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("n", "U", "<C-r>")
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("n", "L", "$")
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>")
 
--- yanky
+vim.keymap.set("n", "<S-l>", ":bn<CR>")
+vim.keymap.set("n", "<S-h>", ":bp<CR>")
+
 vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
 vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
 vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
 vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
