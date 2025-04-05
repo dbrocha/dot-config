9c3cfa9a521d327ac057853c7b404e29c0869b98
dbr <danielbrocha@gmail.com>
2025-04-05 13:25:31 -0400

    .

--- fish/config.fish
+++ fish/config.fish
@@ -13,5 +13,12 @@ set -gx SHELL (which fish)
 starship init fish | source
 zoxide init fish | source
 
-# uv
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
 fish_add_path "/home/azureuser/.local/bin"
